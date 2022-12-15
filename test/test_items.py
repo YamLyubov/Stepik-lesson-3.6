a7d809960cba60e98933a6273555e4abf6ca1356
Lyubov Yamshchikova <43475193+YamLyubov@users.noreply.github.com>
2022-12-15 11:32:25 +0300

    Modify test

--- test/test_items.py
+++ test/test_items.py
@@ -2,7 +2,7 @@ import time
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_add_to_basket_btn_available(browser):
+def test_add_to_basket_btn_available():
     # browser.get(link)
     # button = browser.find_elements('class name', 'btn-add-to-basket')
     assert True
