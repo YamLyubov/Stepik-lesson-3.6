2348a66cf06b97a5d0cbee55c4a685421134bfee
Lyubov Yamshchikova <43475193+YamLyubov@users.noreply.github.com>
2022-12-15 11:20:39 +0300

    Fix test

--- test_items.py
+++ test_items.py
@@ -4,7 +4,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_add_to_basket_btn_available(browser):
     browser.get(link)
-    button = browser.find_elements_by_class_name('btn-add-to-basket')
-    time.sleep(30)
+    button = browser.find_elements('class name', 'btn-add-to-basket')
     assert button, "No button 'add to basket'"
 
